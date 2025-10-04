6ab75708a236fe8da85fe9a81164b256764f048c
hmattam-lgtm <hmattam@colgate.edu>
2025-10-03 23:20:21 -0400

    Done

--- Activity3.R
+++ Activity3.R
@@ -5,23 +5,12 @@
 library(lubridate)
 
 
-
 #create assert function.
 assert <- function(statement,err.message){ 
   if(statement == FALSE){
     print(err.message)
   }
 }
-#Run this function
-assert(1 == 2, "error: unequal values")
-assert(2 == 2, "error: unequal values")
-#check if vectors are same length
-a <- c(1,2,3,4)
-b <- c(8,4,5)
-assert(length(a) == length(b), "error: unequal length")
-
-
-
 
 #read the data
 
@@ -51,53 +40,7 @@ datW$doy <- yday(dates)
 datW$hour <- hour(dates) + (minute(dates)/60)
 #calculate decimal day of year
 datW$DD <- datW$doy + (datW$hour/24)
-#quick preview of new date calculations
-#datW[1,]
 
-
-#check what data is missing
-length(which(is.na(datW$air.temperature))) #air temp
-length(which(is.na(datW$wind.speed))) #wind speed
-length(which(is.na(datW$precipitation))) #precipitation
-length(which(is.na(datW$soil.moisture))) #soil temp
-length(which(is.na(datW$soil.temp))) #soil moisture
-
-
-#make plot for missing points for soil
-plot(datW$DD, datW$soil.moisture, pch=19, type="b", xlab = "Day of Year",
-     ylab="Soil moisture (cm3 water per cm3 soil)")
-#make plot for missing points for air temp
-plot(datW$DD, datW$air.temperature, pch=19, type="b", xlab = "Day of Year",
-     ylab="Air temperature (degrees C)")
-
-#how we can convert unreliable data into NA
-#its saying if th air temp is <0 it becomes NA and goes into air temp
-datW$air.tempQ1 <- ifelse(datW$air.temperature < 0, NA, datW$air.temperature)
-
-#check the values at the extreme range of the data
-quantile(datW$air.tempQ1)
-
-#look at days with really low air temperature
-datW[datW$air.tempQ1 < 8,]  
-
-#look at days with really high air temperature
-datW[datW$air.tempQ1 > 33,] 
-
-#plot precipitation and lightning strikes on the same plot
-#normalize lighting strikes to match precipitation
-lightscale <- (max(datW$precipitation)/max(datW$lightning.acvitivy)) * datW$lightning.acvitivy
-#make the plot with precipitation and lightning activity marked
-#make it empty to start and add in features
-plot(datW$DD , datW$precipitation, xlab = "Day of Year", ylab = "Precipitation & lightning",
-     type="n")
-#plot precipitation points only when there is precipitation 
-#make the points semi-transparent
-points(datW$DD[datW$precipitation > 0], datW$precipitation[datW$precipitation > 0],
-       col= rgb(95/255,158/255,160/255,.5), pch=15)        
-
-#plot lightning points only when there is lightning     
-points(datW$DD[lightscale > 0], lightscale[lightscale > 0],
-       col= "tomato3", pch=19)
 
 ################################################
 ##########         Question 5         ########## 
@@ -182,7 +125,59 @@ points(datW$DD[soiltempValid], tempscale[soiltempValid],
 
 
 
+################################################
+##########         Question 8         ########## 
+################################################
 
+temptot <- sum(!is.na(datW$air.tempQ2))
+totsoiltemp <- sum(!is.na(datW$soil.temp))
+totwindspeed <- sum(!is.na(datW$wind.speed2))
+totsoilmoist <- sum(!is.na(datW$soil.moisture))
+totPercip <- sum(!is.na(datW$precipitation))
+
+start_date <- datW$timestamp[1]
+end_date <- datW$timestamp[nrow(datW)]
+
+table <- data.frame(titles = c("Air Temp", "Wind Speed", "Soil Temp", 
+                               "Soil Moist", "Total Percipitation"),
+  Averages = c(round(mean(datW$air.tempQ2, na.rm = TRUE),1),
+  round(mean(datW$wind.speed2, na.rm = TRUE),2),
+  round(mean(datW$soil.temp, na.rm = TRUE),1),
+  round(mean(datW$soil.moisture, na.rm = TRUE),3),
+  round(sum(datW$precipitation, na.rm = TRUE),3)),
+  Observations = c(temptot,totwindspeed,totsoiltemp,totsoilmoist,totPercip)
+)
+print(paste("Time Period:", start_date, "to", end_date))
+
+
+
+
+################################################
+################################################
+
+
+################################################
+##########         Question 9         ########## 
+################################################
+x_start <- min(datW$DD)
+x_end <- max(datW$DD)
+
+plot(datW$DD, datW$soil.moisture, pch=19, type="b", xlab = "Day of Year",
+     ylab="Soil Moisture (cm3 water per cm3 soil)", 
+     main = "Soil Moisture", xlim = c(x_start,x_end))
+plot(datW$DD, datW$air.tempQ2, pch=19, type="b", xlab = "Day of Year",
+     ylab="Air Temp (Celsius)", 
+     main = "Air Temperature", xlim = c(x_start,x_end))
+plot(datW$DD, datW$soil.temp, pch=19, type="b", xlab = "Day of Year",
+     ylab="Soil Temperature (Celsius)", 
+     main = "Soil Temperature", xlim = c(x_start,x_end))
+plot(datW$DD, datW$precipitation, pch=19, type="b", xlab = "Day of Year",
+     ylab="Precipitation (cm)", 
+     main = "Precipitation", xlim = c(x_start,x_end))
+
+
+################################################
+################################################
 
 
 
