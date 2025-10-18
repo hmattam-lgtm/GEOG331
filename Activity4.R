ddd0f420fd305ca47395123368295e4b2f6dfb7a
hmattam-lgtm <hmattam@colgate.edu>
2025-10-17 20:47:26 -0400

    Part 1 and 2

--- Activity4.R
+++ Activity4.R
@@ -2,12 +2,30 @@ head(iris)
 install.packages(c("tidyverse"))
 library(tidyverse)
 
-
+#subset versicolor
 flower <- iris[iris$Species == "versicolor",]
-vars <- c("flower$Petal.Length","flower$Petal.Length", 
-          "flower$Sepal.Length","flower$Sepal.Width", 
-          "flower$Sepal.Length","flower$Petal.Length")
+
+#list of relationships that we need to make regression tables with
+vars <- list(flower$Sepal.Length ~ flower$Sepal.Width, 
+          flower$Petal.Length ~ flower$Petal.Width, 
+          flower$Sepal.Length ~ flower$Petal.Length)
+
+#empty list to store regressions
 mylist <- list()
-for(i in 1: 6){
-  mylist[[i]] <- lm(vars[i]~vars[i+1])
+
+#empty list to store regression fits
+summ <- list()
+for(i in 1: 3){
+  mylist[[i]] <- lm(vars[[i]])
+  summ[[i]] <- summary(mylist[[i]])
 }
+
+#####################################
+##### Part 2: data in dplyr     #####
+#####################################
+
+#use dplyr to join data of maximum height
+#to a new iris data frame
+height <- data.frame(Species = c("virginica","setosa","versicolor"),
+                     Height.cm = c(60,100,11.8))
+i_height <-dplyr::full_join(iris, height)
