bbbee2d0dca98453f158b1c91ca377addc99ad75
hmattam-lgtm <hmattam@colgate.edu>
2025-10-17 22:03:05 -0400

    Done!

--- Activity4.R
+++ Activity4.R
@@ -2,6 +2,10 @@ head(iris)
 install.packages(c("tidyverse"))
 library(tidyverse)
 
+#####################################
+##### Part 1: for loops         #####
+#####################################
+
 #subset versicolor
 flower <- iris[iris$Species == "versicolor",]
 
@@ -28,4 +32,35 @@ for(i in 1: 3){
 #to a new iris data frame
 height <- data.frame(Species = c("virginica","setosa","versicolor"),
                      Height.cm = c(60,100,11.8))
-i_height <-dplyr::full_join(iris, height)
+
+#this takes all the rows from iris ad adds the height info where species matches
+i_height <-dplyr::left_join(iris, height)
+
+#####################################
+##### Part 3: plots in ggplot2  #####
+#####################################
+
+#look at base R scatter plot
+plot(iris$Sepal.Length,iris$Sepal.Width)
+
+#3a. now make the same plot in ggplot
+#geom adds points and aes is the data
+ggplot(data = iris, mapping = aes(Sepal.Length,Sepal.Width)) + geom_point()
+
+#3b. make a scatter plot with ggplot and get rid of  busy grid lines
+#theme changes the look
+ggplot(data = iris, mapping = aes(Sepal.Length,Sepal.Width)) + geom_point() + theme_classic()
+
+#3c. make a scatter plot with ggplot, remove grid lines, add a title and axis labels, 
+#    show species by color, and make the point size proportional to petal length
+# many variations of aes for color, size, etc and labs for labels
+ggplot(data = iris, mapping = aes(Sepal.Length,Sepal.Width, colour = Species, size = Petal.Length)) + 
+  geom_point() + theme_classic() + labs(x="Sepal Length (cm)", y = "Sepal Width (cm)", title = "Iris Sepal Length by Sepal Width")
+
+
+#####################################
+##### Question: how did         #####
+##### arguments differ between  #####
+##### plot and ggplot?          #####
+#####################################		
+##on google doc
